2ca8dbc8ca861f752b3e2a25fd812abb4ac041bc
osher cohen <oscohen@localhost.localdomain>
2021-01-12 18:39:08 +0200

    Add prefix to process fiealds

--- log_files_to_elastic.py
+++ log_files_to_elastic.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 logging.getLogger("__main__").setLevel(logging.INFO)
 
 MARKED_DIR_PREFIX = "MARK"
-VERSION = 3
+VERSION = 4
 INDEX = "ai_events"
 MARKED_DIR = "{}_{}".format(MARKED_DIR_PREFIX, VERSION)
 
--- process.py
+++ process.py
@@ -4,9 +4,9 @@ def process_metadata(json_file):
     if "cluster_hosts" in json_file:
         host_jsons = json_file["cluster_hosts"]
 
-        json_file["hosts_count"]            =  len(host_jsons)
-        json_file["hosts_synced_ntp_count"] = len(["*" for host in host_jsons if  "Host NTP is synced" in host['validations_info']])
-        json_file["all_hosts_ntp_synced"]   = json_file["hosts_count"] == json_file["hosts_synced_ntp_count"]
+        json_file["process_hosts_count"]            = len(host_jsons)
+        json_file["process_hosts_synced_ntp_count"] = len(["*" for host in host_jsons if  "Host NTP is synced" in host['validations_info']])
+        json_file["process_cluster_ntp_synced"]     = json_file["process_hosts_count"] == json_file["process_hosts_synced_ntp_count"]
 
     return json_file
 
